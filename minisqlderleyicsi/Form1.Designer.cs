011aa590066feb37c5a47cac00e6ebe42254fc49
Serhat <serhattdemirr2005@gmail.com>
2024-11-10 09:51:12 +0300

    Proje dosyası ekle.

---
+++ minisqlderleyicsi/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace minisqlderleyicsi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ScrollBar;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 317);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(998, 298);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(761, 300);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(778, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(369, 121);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ÇALIŞTIR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(774, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Çalışmak istediğniz veri tabanını seçin:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(778, 182);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 30);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(818, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tablolar:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(911, 225);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 92);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1159, 619);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ minisqlderleyicsi/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace minisqlderleyicsi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+            veritabanliste();
+            listBox1.Items.Clear();
+        }
+        //LOAD BOŞ GELMESİN DİYE HAZIRDA OLAN BİR VERİTABANIYLA TABLOSUNU ÇAĞIRIYORUM. 
+        //PROGRAM BU METODU SADECE İLK AÇILIŞTA KULLANMIŞ OLUYOR. YANİ OLMASA DA OLURDU.
+        void listele()
+        {
+            SqlConnection baglanti = new SqlConnection(@"Data Source=SerhatDemir\SQLEXPRESS;Initial Catalog=DbNotKayıt;Integrated Security=True");
+            SqlDataAdapter da = new SqlDataAdapter("select * from TBLDERS", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        //DİNAMİK OLARAK ÜRETİLEN BAĞLANTI NESNESİNDE VERİTABANI ADINDAKİ DEĞİŞKENLE BİRLİKTE
+        //SİSTEMDEKİ TÜM VERİTABANLARI ÇAĞIRILIP COMBOBOX DATASOURCE UNA AKTARILIYOR.
+        void veritabanliste()
+        {
+            SqlConnection baglanti = new SqlConnection(@"Data Source=SerhatDemir\SQLEXPRESS;Initial Catalog=;Integrated Security=True");
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT NAME FROM sys.databases", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            comboBox1.DisplayMember = "NAME";
+            comboBox1.DataSource = dt;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            //COMBOBOXDAN VERİTABANI SEÇİMİ YAPILDIĞINDA SEÇİLEN VERİTABANI ADINI
+            //YENİ OLUŞTURULAN BAĞLANTI NESNESİNE AKTARARAK SEÇİLEN VERİTABANINI ÇAĞIRIP
+            //ÇAĞIRILAN VERİTABANINA AİT TABLOLARI GETİRİYORUZ 
+            //VE BUNLARI LISTBOXITEMS OLARAK YAZDIRIYORUZ.
+            SqlConnection baglanti = new SqlConnection(@"Data Source=SerhatDemir\SQLEXPRESS;Initial Catalog=" + comboBox1.Text + ";Integrated Security=True");
+            baglanti.Open();
+            SqlCommand cmd2 = new SqlCommand("SELECT NAME FROM sys.tables", baglanti);
+            SqlDataReader dr = cmd2.ExecuteReader();
+            while (dr.Read())
+            {
+                listBox1.Items.Add(dr[0]);
+            }
+            baglanti.Close();
+            //veritabani = comboBox1.Text;
+            label1.Text = comboBox1.Text;
+        }
+        string listboxitemsec = "";
+
+        //SORGU BUTONU
+        string sorgu;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlbaglanti = new SqlConnection(@"Data Source=SerhatDemir\SQLEXPRESS;Initial Catalog=" + comboBox1.Text + ";Integrated Security=True");
+
+            void liste()
+            {
+                //LISTBOXITEMSEC ADINDA BİR DEĞİŞKEN TANIMLAYIP LİSTBOXDAN
+                //SEÇİLEN İTEMA GÖRE LİSTELEME YAPAN SORGU.
+                SqlDataAdapter listeleadapter = new SqlDataAdapter("select * from " + listboxitemsec, sqlbaglanti);
+                DataTable listeletable = new DataTable();
+                listeleadapter.Fill(listeletable);
+                dataGridView1.DataSource = listeletable;
+            }
+            sorgu = richTextBox1.Text;
+            //BURADA DA SORGU İÇİNDE DE DML KOMUTLARI VAR MI YOK MU BAKTIRIP ONA GÖRE İŞLEM YAPTIRIYORUZ.
+            if (sorgu.Contains("select"))
+            {
+               
+                SqlDataAdapter da = new SqlDataAdapter(sorgu, sqlbaglanti);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+
+            if (sorgu.Contains("insert"))
+            {
+                try
+                {
+                    sqlbaglanti.Open();
+                    SqlCommand cmd = new SqlCommand(sorgu, sqlbaglanti);
+                    cmd.ExecuteNonQuery();
+                    sqlbaglanti.Close();
+                    liste();
+                    MessageBox.Show("Insert sorgunuz başarılı şekilde gerçekleştirildi.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Insert sorgunuzu kontrol edin.", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            if (sorgu.Contains("update"))
+            {
+                try
+                {
+                    sqlbaglanti.Open();
+                    SqlCommand cmd = new SqlCommand(sorgu, sqlbaglanti);
+                    cmd.ExecuteNonQuery();
+                    sqlbaglanti.Close();
+                    liste();
+                    MessageBox.Show("Update sorgunuz başarılı şekilde gerçekleştirildi.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Update sorgunuzu kontrol edin.", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            if (sorgu.Contains("delete"))
+            {
+                try
+                {
+                    sqlbaglanti.Open();
+                    SqlCommand cmd = new SqlCommand(sorgu, sqlbaglanti);
+                    cmd.ExecuteNonQuery();
+                    sqlbaglanti.Close();
+                    liste();
+                    MessageBox.Show("Delete sorgunuz başarılı şekilde gerçekleştirildi.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Delete sorgunuzu kontrol edin.", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection sqlbaglanti = new SqlConnection(@"Data Source=SerhatDemir\SQLEXPRESS;Initial Catalog=" + comboBox1.Text + ";Integrated Security=True");
+            listboxitemsec = listBox1.SelectedItems[0].ToString();
+            SqlDataAdapter listeleadapter = new SqlDataAdapter("select * from " + listboxitemsec, sqlbaglanti);
+            DataTable listeletable = new DataTable();
+            listeleadapter.Fill(listeletable);
+            dataGridView1.DataSource = listeletable;
+        }
+
+    }
+}
